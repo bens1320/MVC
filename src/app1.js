9c627cf0d06a579ea6bb2edd3fee83b92754e01d
bens1320 <yu1piao09@163.com>
2020-07-08 03:43:10 +0800

    MVC2

--- src/app1.js
+++ src/app1.js
@@ -1,39 +1,101 @@
 import "./app1.css"
 import $ from "jquery"
 
-const $button1 = $("#add1")
-const $button2 = $("#subtract")
-const $button3 = $("#multiply")
-const $button4 = $("#divide")
-const $number = $("#number")
+const eventBus = $(window)
+// 数据相关: m
+const m = {
+    //初始化数据
+    data: {
+        n: parseInt(localStorage.getItem("n")) || 100
+    },
+    create(){},
+    delete(){},
+    update(data){
+        //把data所有属性赋值给m.data
+        Object.assign(m.data, data)
+        //eventBus.trigger不能用空格
+        eventBus.trigger("m:updated")
+        localStorage.setItem("n", m.data.n)
+    },
+    get(){}
+}
 
-const n = localStorage.getItem("n")
-$number.text(n | 100)
+// 视图相关:v
+const v = {
+    el: null,
+    html: `
+    <div>
+        <div class="output">
+            <span id="number">{{n}}</span>
+        </div>
+        <div class="actions">
+            <button id="add1">+1</button>
+            <button id="subtract">-1</button>
+            <button id="multiply">*2</button>
+            <button id="divide">/2</button>
+        </div>
+    </div>
+    `,
+    init(container){
+        v.el = $(container)
+    },
+    render(n){
+        if (v.el.children().length !== 0) {
+            v.el.empty()
+        }
+        $(v.html.replace("{{n}}", n))
+            .appendTo($(v.el))
+    },
+}
 
-$button1.on("click", () => {
-    let n = parseInt($number.text())
-    n += 1
-    localStorage.setItem("n", n)
-    $number.text(n)
-})
+// 其他: c
+const c = {
+    init(container){
+        v.init(container)
+        // view = render(data)
+        v.render(m.data.n)
+        c.autoBindEvents()
+        eventBus.on("m:updated", () => {
+            v.render(m.data.n)
+        })
+    },
+    events: {
+        "click #add1": "add",
+        "click #subtract": "sub",
+        "click #multiply": "multi",
+        "click #divide": "div"
+    },
+    add(){
+        m.update({n: m.data.n + 1})
+    },
 
-$button2.on("click", () => {
-    let n = parseInt($number.text())
-    n -= 1
-    localStorage.setItem("n", n)
-    $number.text(n)
-})
+    sub(){
+        m.update({n: m.data.n - 1})
+    },
 
-$button3.on("click", () => {
-    let n = parseInt($number.text())
-    n *= 2
-    localStorage.setItem("n", n)
-    $number.text(n)
-})
+    multi(){
+        m.update({n: m.data.n * 2})
+    },
 
-$button4.on("click", () => {
-    let n = parseInt($number.text())
-    n /= 2
-    localStorage.setItem("n", n)
-    $number.text(n)
-})
+    div(){
+        m.update({n: m.data.n / 2})
+    },
+
+    autoBindEvents(){
+        for (let key in c.events) {
+            const value = c[c.events[key]]
+            const spaceIndex = key.indexOf(" ")
+            const part1 = key.slice(0, spaceIndex)
+            const part2 = key.slice(spaceIndex + 1)
+            v.el.on(part1, part2, value)
+        }
+    }
+}
+
+export default c
+
+
+
+
+
+
--- src/app3.js
+++ src/app3.js
@@ -1,7 +1,27 @@
 import "./app3.css"
 import $ from "jquery"
 
-const $square = $("#app3 .square")
-$square.on("click", () => {
-    $square.toggleClass("active")
+const html = `
+    <section id="app3">
+        <div class="square"></div>
+    </section>
+`
+
+const $element = $(html).appendTo($("body > .page"))
+
+const $square = $('#app3 .square')
+const localKey = 'app3.active'
+// yes no undefined
+const active = localStorage.getItem(localKey) === 'yes'
+
+$square.toggleClass('active', active)
+
+$square.on('click', () => {
+    if ($square.hasClass('active')) {
+        $square.removeClass('active')
+        localStorage.setItem(localKey, 'no')
+    } else {
+        $square.addClass('active')
+        localStorage.setItem('app3.active', 'yes')
+    }
 })
