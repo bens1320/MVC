2eb18a4beccbe329a6d9804762e6eb2d4273dfe3
bens1320 <yu1piao09@163.com>
2020-07-17 03:38:31 +0800

    MVC版->完整版vue重构

--- src/app1.js
+++ src/app1.js
@@ -1,71 +1,52 @@
 import "./app1.css"
-import $ from "jquery"
-import Model from "../base/Model";
-import View from "../base/View";
-
-// 数据相关: m
-const m = new Model({
-    data: {
-        n: parseFloat(localStorage.getItem("n")) || 100,
-    },
-    update(data){
-        //把data所有属性赋值给m.data
-        Object.assign(m.data, data)
-        //eventBus.trigger不能用空格
-        m.trigger("m:updated")
-        localStorage.setItem("n", m.data.n)
-    }
-})
-
-
+import Vue from 'vue/dist/vue.esm.js'
 
 // 其他: c
 const init = (el) => {
-    new View({
+    new Vue({
         el: el,
-        data: m.data,
-        html: `
-            <div>
+        data:{
+            n: parseFloat(localStorage.getItem("n")) || 100
+        },
+        methods: {
+            add(){
+                this.n += 1
+            },
+
+            sub(){
+                this.n -= 1
+            },
+
+            multi(){
+                this.n *= 2
+            },
+
+            div(){
+                this.n /= 2
+            },
+        },
+        watch: {
+            // n: function(){
+            //     localStorage.setItem("n", this.n)
+            // }
+            n(){
+                localStorage.setItem("n", this.n)
+            }
+        },
+        template:`
+            <!-- vue 会吧当前div替换之前的 -->
+            <section id="app1">
                 <div class="output">
                     <span id="number">{{n}}</span>
                 </div>
                 <div class="actions">
-                    <button id="add1">+1</button>
-                    <button id="subtract">-1</button>
-                    <button id="multiply">*2</button>
-                    <button id="divide">/2</button>
+                    <button @click="add">+1</button>
+                    <button @click="sub">-1</button>
+                    <button @click="multi">*2</button>
+                    <button @click="div">/2</button>
                 </div>
-            </div> 
-        `,
-        render(data){
-            const n = data.n
-            if (this.el.children().length !== 0) {
-                this.el.empty()
-            }
-            $(this.html.replace("{{n}}", n))
-                .appendTo($(this.el))
-        },
-        events: {
-            "click #add1": "add",
-            "click #subtract": "sub",
-            "click #multiply": "multi",
-            "click #divide": "div"
-        },
-        add(){
-            m.update({n: m.data.n + 1})
-        },
-
-        sub(){
-            m.update({n: m.data.n - 1})
-        },
-
-        multi(){
-            m.update({n: m.data.n * 2})
-        },
-
-        div(){
-            m.update({n: m.data.n / 2})
-        }
+            </section> 
+        `
     })
 }
 
--- src/app2.js
+++ src/app2.js
@@ -1,62 +1,30 @@
 import "./app2.css"
-import $ from "jquery"
-import Model from "../base/Model";
-import View from "../base/View";
-import EventBus from "../base/EventBus";
-
-const eventBus = new EventBus()
-
-const localKey = "app2.index"
-// 数据相关: m
-const m = new Model({
-    //初始化数据
-    data: {
-        index: parseInt(localStorage.getItem(localKey)) || 0
-    },
-    update(data){
-        //把data所有属性赋值给m.data
-        Object.assign(m.data, data)
-        //eventBus.trigger不能用空格
-        eventBus.trigger("m:updated")
-        localStorage.setItem(localKey, m.data.index)
-    },
-})
+import Vue from 'vue/dist/vue.esm.js'
 
 // 其他: c
 const init = (el) => {
-    new View({
+    new Vue({
         el: el,
-        data: m.data,
-        eventBus: eventBus,
-        html:(index) => {
-            return`
-        <div>
+        data: {
+            index: parseInt(localStorage.getItem("index")) || 0
+        },
+        template: `
+          <section id="app2">
             <ol class="tab-bar">
-                <li class="${ index === 0 ? 'selected' : ''}" data-index="0">1111</li>
-                <li class="${ index === 1 ? 'selected' : ''}" data-index="1"><span>2222</span></li>
+                <li :class="index === 0 ? 'selected' : ''" @click="index=0" data-index="0">1111</li>
+                <li :class="index === 1 ? 'selected' : ''" @click="index=1" data-index="1"><span>2222</span></li>
             </ol>
             <ol class="tab-content">
-                <li class="${ index === 0 ? 'active' : ''}">内容1</li>
-                <li class="${ index === 1 ? 'active' : ''}">内容2</li>
+                <li :class="index === 0 ? 'active' : ''">内容1</li>
+                <li :class="index === 1 ? 'active' : ''">内容2</li>
             </ol>
-        </div>
-        `
-        },
-        render(data){
-            const index = data.index
-            if (this.el.children().length !== 0) {
-                this.el.empty()
+        </section>
+        `,
+        watch: {
+            index(){
+                localStorage.setItem("index", this.index)
             }
-            $(this.html(index )).appendTo($(this.el))
-        },
-        events: {
-            "click .tab-bar li": "x",
-        },
-        x(e){
-            const index = parseInt(e.currentTarget.dataset.index )
-            m.update({index: index})
-        },
-
+        }
     })
 }
 export default init
